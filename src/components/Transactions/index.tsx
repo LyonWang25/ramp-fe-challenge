5f75f843555ea00a26bdeef69e756dfb310f41d6
Lyon <t41589@gmail.com>
2023-08-17 15:45:24 -0400

    Fixed Bug 7: Approving a transaction won't persist the new value

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -1,36 +1,60 @@
-import { useCallback } from "react"
-import { useCustomFetch } from "src/hooks/useCustomFetch"
-import { SetTransactionApprovalParams } from "src/utils/types"
-import { TransactionPane } from "./TransactionPane"
-import { SetTransactionApprovalFunction, TransactionsComponent } from "./types"
+import { useCallback, useEffect, useState } from 'react';
+import { useCustomFetch } from 'src/hooks/useCustomFetch';
+import { SetTransactionApprovalParams, Transaction } from 'src/utils/types';
+import { TransactionPane } from './TransactionPane';
+import { SetTransactionApprovalFunction, TransactionsComponent } from './types';
 
-export const Transactions: TransactionsComponent = ({ transactions }) => {
-  const { fetchWithoutCache, loading } = useCustomFetch()
+export const Transactions: TransactionsComponent = ({
+  transactions: initialTransactions,
+}) => {
+  console.log('initialTransactions', initialTransactions); //initialTransactions (5) [{…}, {…}, {…}, {…}, {…}]
+  const { fetchWithoutCache, loading } = useCustomFetch();
+  const [transactions, setTransactions] = useState<Transaction[]>(
+    initialTransactions as Transaction[]
+  );
+  useEffect(() => {
+    setTransactions(initialTransactions as Transaction[]);
+  }, [initialTransactions]);
 
   const setTransactionApproval = useCallback<SetTransactionApprovalFunction>(
     async ({ transactionId, newValue }) => {
-      await fetchWithoutCache<void, SetTransactionApprovalParams>("setTransactionApproval", {
-        transactionId,
-        value: newValue,
-      })
+      await fetchWithoutCache<void, SetTransactionApprovalParams>(
+        'setTransactionApproval',
+        {
+          transactionId,
+          value: newValue,
+        }
+      );
+      // Update the local state with the new approval status
+      setTransactions((prevTransactions) => {
+        return prevTransactions.map((transaction) =>
+          transaction.id === transactionId
+            ? { ...transaction, approved: newValue }
+            : transaction
+        );
+      });
     },
     [fetchWithoutCache]
-  )
+  );
 
   if (transactions === null) {
-    return <div className="RampLoading--container">Loading...</div>
+    return <div className="RampLoading--container">Loading...</div>;
   }
 
   return (
     <div data-testid="transaction-container">
-      {transactions.map((transaction) => (
-        <TransactionPane
-          key={transaction.id}
-          transaction={transaction}
-          loading={loading}
-          setTransactionApproval={setTransactionApproval}
-        />
-      ))}
+      {transactions ? (
+        transactions.map((transaction) => (
+          <TransactionPane
+            key={transaction.id}
+            transaction={transaction}
+            loading={loading}
+            setTransactionApproval={setTransactionApproval}
+          />
+        ))
+      ) : (
+        <div>No transactions available</div>
+      )}
     </div>
-  )
-}
+  );
+};
